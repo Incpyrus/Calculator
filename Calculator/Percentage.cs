7b347c2a571db9350c3f2775e836a4dc5d79c287
Incpyrus <incrediblepyrus@gmail.com>
2018-08-11 00:13:20 -0400

    Add Percentage

---
+++ Calculator/Percentage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Calculator
+{
+    public class Percentage
+    {
+        public double Percent(double x, double y)
+        {
+            return x/y*100;
+        }
+    }
+}
